ba3e3fa93a0ffe1ea9d4454f7df8fa0786995ba8
root <root@ip-10-0-43-44.ec2.internal>
2024-02-16 15:01:58 -0500

    first commit

---
+++ nginx_adddomain/addstore.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+dominio=$1
+version=$2
+dominio_publico=''
+
+read -r -d '' CONFIG <<EOF
+server {
+    listen 80;
+    server_name $dominio www.$dominio;
+    # Redirige HTTP a HTTPS
+    return 301 https://\$host\$request_uri;
+}
+server {
+    listen 443 ssl;
+    server_name $dominio;
+    ssl_certificate /etc/letsencrypt/live/luchosuarez.shop/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/luchosuarez.shop/privkey.pem; # managed by Certbot
+    return 301 https://www.$dominio\$request_uri;
+}
+server {
+    listen 443 ssl;
+    server_name www.$dominio;
+    ssl_certificate /etc/letsencrypt/live/luchosuarez.shop/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/luchosuarez.shop/privkey.pem; # managed by Certbot
+    include /etc/nginx/sites-available/aaa-master-aws-sami$version;
+}
+EOF
+
+
+
+if [ $version == 5 ]
+then
+    dominio_publico=$dominio-sami5.conf
+    cd /etc/nginx/sites-available/ && echo "$CONFIG" > $dominio-sami5.conf
+    echo " Creado el archivo $dominio-sami5.conf"
+elif [ $version == 3 ]
+then
+    dominio_publico=$dominio.conf
+    cd /etc/nginx/sites-available/ && echo "$CONFIG" > $dominio.conf
+    echo "Creado el archivo $dominio.conf"
+fi
+
+echo "Creando la referencia..."
+ln -s /etc/nginx/sites-available/$dominio_publico /etc/nginx/sites-enabled/
+echo "Creada la referencia con exito"
+
+nginx -t 
+sleep 60
+
+certbot --nginx -d $dominio -d www.$dominio
